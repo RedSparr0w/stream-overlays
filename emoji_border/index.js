01a1a65cdd5844c588d666d530bf35ff93ba1085
Danial <admin@redsparr0w.com>
2022-02-14 17:27:22 +1300

    initial

---
+++ emoji_border/index.js
@@ -0,0 +1,60 @@
+
+
+/* Changing css variables */
+const rootStyle = document.querySelector(':root').style;
+const setCSS = (property, value) => {
+  rootStyle.setProperty(property, value);
+}
+
+/* Adding hearts */
+const addHearts = (side) => {
+  var size = Math.floor(Math.random() * 20) + 20;
+  var offset = Math.random() * 100;
+  const rotation = (Math.random() * 30) - 15;
+
+  const heart = document.createElement('div');
+  heart.className = 'heart';
+  heart.style.fontSize = `${size}px`;
+  heart.style.transform = `rotate(${rotation}deg)`;
+
+  switch (side) {
+    case 'top':
+    case 'bottom':
+      heart.style.left = `${offset}%`;
+      break;
+    case 'left':
+    case 'right':
+      heart.style.top = `${offset}%`;
+      break;
+  }
+
+  switch (side) {
+    case 'top':
+      heart.style.top = '0px';
+      break;
+    case 'bottom':
+      heart.style.bottom = '10px';
+      break;
+    case 'left':
+      heart.style.left = `${Math.random() * 25}px`;
+      break;
+    case 'right':
+      heart.style.right = `${Math.random() * 25}px`;
+      break;
+  }
+
+  document.body.appendChild(heart);
+}
+
+for (let i = 0; i < window.innerWidth / 12; i++) {
+  setTimeout(() => addHearts('top'), i * 66);
+}
+for (let i = 0; i < window.innerHeight / 12; i++) {
+  setTimeout(() => addHearts('bottom'), i * 66);
+}
+for (let i = 0; i < window.innerHeight / 35; i++) {
+  setTimeout(() => addHearts('left'), i * 66);
+}
+for (let i = 0; i < window.innerHeight / 35; i++) {
+  setTimeout(() => addHearts('right'), i * 66);
+}
