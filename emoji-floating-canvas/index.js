fde15e9933e377c4599a3dcd57dfa82bb9ec4db8
Danial <admin@redsparr0w.com>
2022-03-06 16:05:03 +1300

    Update canvas

--- emoji-floating-canvas/index.js
+++ emoji-floating-canvas/index.js
@@ -1,148 +1,162 @@
-const particleOptions = {
+// Our default settings
+const Settings = {
+  // Movement
+  render_time: 20,
   velocity_x: 0,
-  velocity_y: -10,
+  velocity_y: -3,
+  gravity: -0.02,
+  // Display
+  text: '❤❤xo',
+  color: [244, 0, 147],
+  size: 40,
+  size_variance: 15,
+  amount: 200,
+  max_age: 3000,
+  fade_in: 20,
+  fade_out: 80,
 }
 
-// Creating a GUI with options.
+// To get our user specified values
+const PageParams = new URLSearchParams(window.location.search);
+Object.keys(Settings).forEach(k => {
+  try {
+    const value = PageParams.get(k);
+    if (value != undefined) Settings[k] = JSON.parse(PageParams.get(k));
+  }catch(e){}
+});
+UpdateURI = () => {
+  const params = {};
+  Object.keys(Settings).forEach(k => {
+    params[k] = JSON.stringify(Settings[k]);
+  });
+  const uri = `${location.origin}${location.pathname}?${new URLSearchParams(params).toString()}`;
+  history.replaceState({}, undefined, uri);
+}
+
+
+// Creating a GUI with our settings
 const GUI = new dat.GUI({name: 'Particles controls'});
 
 const Movement = GUI.addFolder('Movement');
-Movement.add(particleOptions, 'velocity_x', -100, 100);
-Movement.add(particleOptions, 'velocity_y', -100, 100);
+Movement.add(Settings, 'render_time', 1, 50).onChange(UpdateURI);
+Movement.add(Settings, 'velocity_x', -10, 10, 0.01).onChange(UpdateURI);
+Movement.add(Settings, 'velocity_y', -10, 10, 0.01).onChange(UpdateURI);
+Movement.add(Settings, 'gravity', -1, 1, 0.01).onChange(UpdateURI);
 
-window.onload = function() {
+const Display = GUI.addFolder('Display');
+Display.add(Settings, 'text').onChange(UpdateURI);
+Display.addColor(Settings, 'color').onChange(UpdateURI);
+Display.add(Settings, 'size', 5, 100).onChange(UpdateURI);
+Display.add(Settings, 'size_variance', 0, 50).onChange(UpdateURI);
+Display.add(Settings, 'amount', 1, 1000).onChange(UpdateURI);
+Display.add(Settings, 'max_age', 500, 10000).onChange(UpdateURI);
+Display.add(Settings, 'fade_in', 0, 100).onChange(v => {
+  if (v > Settings.fade_out) Settings.fade_out = v;
+  GUI.updateDisplay();
+  UpdateURI();
+});
+Display.add(Settings, 'fade_out', 0, 100).onChange(v => {
+  if (v < Settings.fade_in) Settings.fade_in = v;
+  GUI.updateDisplay();
+  UpdateURI();
+});
+
+
+// Adding the particles
+window.onload = () => {
   // Initialise an empty canvas and place it on the page
-  var canvas = document.createElement("canvas");
-  var context = canvas.getContext("2d");
+  const canvas = document.createElement("canvas");
+  const context = canvas.getContext("2d");
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
   document.body.appendChild(canvas);
 
   // No longer setting velocites as they will be random
   // Set up object to contain particles and set some default values
-  var particles = {},
-      particleIndex = 0,
-      settings = {
-        density: 1,
-        particleSize: 5,
-        startingX: canvas.width / 2,
-        startingY: canvas.height / 4,
-        gravity: 0.5,
-        maxLife: 150,
-        groundLevel: canvas.height,
-        leftWall: canvas.width * 0.1,
-        rightWall: canvas.width
-      };
-
-  // To optimise the previous script, generate some pseudo-random angles
-  var seedsX = [];
-  var seedsY = [];
-  var maxAngles = 100;
-  var currentAngle = 0;
-
-  function seedAngles() {
-    seedsX = [];
-    seedsY = [];
-    for (var i = 0; i < maxAngles; i++) {
-      seedsX.push(Math.random() * 20 - 10);
-      seedsY.push(Math.random() * 30 - 10);
-    }
-  }
-
-  // Start off with 100 angles ready to go
-  seedAngles();
+  let particles = {},
+      particleIndex = 0;
 
   // Set up a function to create multiple particles
   function Particle() {
-    if (currentAngle !== maxAngles) {
-      // Establish starting positions and velocities
-      this.x = settings.startingX;
-      this.y = settings.startingY;
+    this.text = Settings.text.split('')[Math.floor(Math.random() * Settings.text.length)];
 
-      this.vx = seedsX[currentAngle];
-      this.vy = seedsY[currentAngle];
+    // Establish starting positions and velocities
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
 
-      currentAngle++;
+    this.vx = Settings.velocity_x * Settings.render_time / 100;
+    this.vy = Settings.velocity_y * Settings.render_time / 100;
 
-      // Add new particle to the index
-      // Object used as it's simpler to manage that an array
-      particleIndex ++;
-      particles[particleIndex] = this;
-      this.id = particleIndex;
-      this.life = 0;
-      this.maxLife = settings.maxLife;
-    } else {
-      console.log('Generating more seed angles');
-      seedAngles();
-      currentAngle = 0;
-    }
+    this.size = Settings.size + (Math.random() - 0.5) * (Settings.size_variance * 2);
+    this.opacity = 0;
+    this.fade_in = (Settings.max_age/100) * Settings.fade_in;
+    this.fade_in_amount = 1 / (this.fade_in / Settings.render_time);
+    this.fade_out = (Settings.max_age/100) * Settings.fade_out;
+    this.fade_out_amount = 1 / ((Settings.max_age - this.fade_out) / Settings.render_time);
+
+    this.id = particleIndex;
+    this.age = 0;
+    this.delay = Math.random() * Settings.max_age;
+
+    // Add new particle to the index
+    particles[particleIndex++] = this;
   }
 
   // Some prototype methods for the particle's "draw" function
   Particle.prototype.draw = function() {
+    if (this.delay > 0) {
+      this.delay -= Settings.render_time;
+      return;
+    }
     this.x += this.vx;
     this.y += this.vy;
     
-    // Give the particle some bounce
-    if ((this.y + settings.particleSize) > settings.groundLevel) {
-      this.vy *= -0.6;
-      this.vx *= 0.75;
-      this.y = settings.groundLevel - settings.particleSize;
-    }
-
-    // Determine whether to bounce the particle off a wall
-    if (this.x - (settings.particleSize) <= settings.leftWall) {
-      this.vx *= -1;
-      this.x = settings.leftWall + (settings.particleSize);
-    }
-
-    if (this.x + (settings.particleSize) >= settings.rightWall) {
-      this.vx *= -1;
-      this.x = settings.rightWall - settings.particleSize;
-    }
+    // // Give the particle some bounce
+    // if ((this.y - this.size) < 0) {
+    //   this.vy *= -0.8;
+    //   this.y = this.size;
+    // }
 
     // Adjust for gravity
-    this.vy += settings.gravity;
+    this.vy += Settings.gravity * Settings.render_time / 100;
 
     // Age the particle
-    this.life++;
+    this.age += Settings.render_time;
 
     // If Particle is old, it goes in the chamber for renewal
-    if (this.life >= this.maxLife) {
+    if (this.age >= Settings.max_age || (this.y + this.size) < 0) {
       delete particles[this.id];
     }
 
-    // Create the shapes
-    //context.fillStyle = "red";
-    //context.fillRect(this.x, this.y, settings.particleSize, settings.particleSize);
-    context.clearRect(settings.leftWall, settings.groundLevel, canvas.width, canvas.height);
-    context.beginPath();
-    context.fillStyle="#0000ff";
-    // Draws a circle of radius 20 at the coordinates 100,100 on the canvas
-    context.arc(this.x, this.y, settings.particleSize, 0, Math.PI*2, true); 
-    context.closePath();
-    context.fill();
+    // // Create the shapes
+    // context.beginPath();
+    // context.fillStyle="#0000ff";
+    // // Draws a circle of radius 20 at the coordinates 100,100 on the canvas
+    // context.arc(this.x, this.y, this.size, 0, Math.PI*2, true); 
+    // context.closePath();
+    // context.fill();
+    context.font = `${this.size}px Arial`;
+    context.fillStyle = `rgba(${Settings.color[0]}, ${Settings.color[1]}, ${Settings.color[2]}, ${this.age <= this.fade_in ? this.opacity += this.fade_in_amount : this.age >= this.fade_out ? this.opacity -= this.fade_out_amount : this.opacity})`;
+    context.textAlign = 'center';
+    context.fillText(this.text, this.x, this.y);
   }
 
-  setInterval(function() {
-    context.fillStyle = "rgba(10,10,10,0.8)";
-    context.fillRect(0, 0, canvas.width, canvas.height);
-
-    // Draw a left, right walls and floor
-    context.fillStyle = "white";
-    context.fillRect(0, 0, settings.leftWall, canvas.height);
-    context.fillRect(settings.rightWall, 0, canvas.width, canvas.height);
-    context.fillRect(0, settings.groundLevel, canvas.width, canvas.height);
+  drawCanvas = () => {
+    // Fill the backgorund
+    // context.fillStyle = "rgba(20,20,20,0.8)";
+    // context.fillRect(0, 0, canvas.width, canvas.height);
+    // Transparent background
+    context.clearRect(0, 0, canvas.width, canvas.height);
 
     // Draw the particles
-    for (var i = 0; i < settings.density; i++) {
+    while (Object.values(particles).length < Settings.amount) {
       new Particle();
     }
 
     for (var i in particles) {
       particles[i].draw();
     }
-  }, 30);
-
-
+    setTimeout(drawCanvas, Settings.render_time);
+  }
+  drawCanvas();
 };
